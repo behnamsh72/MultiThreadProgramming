7d14d5b16c6949a5d8e9692c728b87a1150d0dab
behnamsh72 <Behnamshbehnam72>
2019-07-14 20:24:58 +0430

    Thread Factory

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,18 @@
-
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        int numberOfThreads = 2 * Runtime.getRuntime().availableProcessors();
-        MyThreadGroup threadGroup = new MyThreadGroup("My ThreadGroup");
+        MyThreadFactory factory = new MyThreadFactory("My Thread Factory");
         Task task = new Task();
-
-        //Create the thread objects associated to the thread group
-        for (int i = 0; i < numberOfThreads; i++) {
-            Thread t = new Thread(threadGroup, task);
-            t.start();
+        Thread thread;
+        System.out.printf("Starting the Threads\n");
+        for (int i = 0; i < 10; i++) {
+            thread = factory.newThread(task);
+            thread.start();
         }
 
-        System.out.printf("Number of threads: %d\n", threadGroup.activeCount());
-        System.out.printf("Information about the thread group\n");
-        threadGroup.list();
-
-        //Write information about the status of the Thread objects to the console
-
-        Thread[] threads = new Thread[threadGroup.activeCount()];
-        threadGroup.enumerate(threads);
-        for (int i = 0; i < threadGroup.activeCount(); i++) {
-            System.out.printf("Thread %s: %s\n", threads[i].getName(), threads[i].getState());
-        }
-
+        System.out.printf("Factory Stats:\n");
+        System.out.printf("%s\n", factory.getStats());
 
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,19 +1,12 @@
-import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Task implements Runnable {
     @Override
     public void run() {
-
-        int result;
-        Random random = new Random(Thread.currentThread().getId());
-        while (true) {
-            //Generate a random number and calculate 1000000000 divide by that random number
-            result = 1000 / ((int) (random.nextDouble() * 1000000000));
-            //check if the thread has been interrupted
-            if (Thread.currentThread().isInterrupted()) {
-                System.out.printf("%d : interrupted\n", Thread.currentThread().getId());
-                return;
-            }
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
